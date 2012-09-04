e8143872d9aef9613958869cfbbfdde09bac26c9
Herve Bredin <bredin@limsi.fr>
2012-09-04 10:36:16 +0200

    Updated sd_sid_cross.sh with input list of files

--- sd_sid_cross.sh
+++ sd_sid_cross.sh
@@ -8,18 +8,32 @@
 SID=/people/barras/diarization/scripts/diarization-sid12.sh
 SEG2MDTM=/people/barras/diarization/scripts/seg2mdtm
 
-EXPECTED_ARGS=3
+EXPECTED_ARGS=4
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
     echo "What? diarization-sid12.sh -ubmset pakita -iter 5 -stop 1.0 -cross -plpdir plpDir "
-    echo "                           ${INPUT_DIR}/%s.wav ${OUTPUT_DIR}/all ${OUTPUT_DIR}/*.stage1.seg"
-    echo "How? qsub `basename $0` wavDir plpDir outputDir"
+    echo "                           ${INPUT_DIR}/%s.wav ${OUTPUT_DIR}/listName ${OUTPUT_DIR}/*.stage1.seg"
+    echo "How? qsub `basename $0` list wavDir plpDir outputDir"
 else
-    INPUT_DIR=$1
-    PLP_DIR=$2
-    OUTPUT_DIR=$3
-    ${SID} -ubmset pakita -iter 5 -stop 1.0 -cross -plpdir ${PLP_DIR} ${INPUT_DIR}/%s.wav ${OUTPUT_DIR}/all ${OUTPUT_DIR}/*.stage1.seg
-    ${SEG2MDTM} < ${OUTPUT_DIR}/all/cross.stop1.0.seg > ${OUTPUT_DIR}/all/diarization.mdtm
+    LIST=$1
+    INPUT_DIR=$2
+    PLP_DIR=$3
+    OUTPUT_DIR=$4
+    
+    # Create combined listName/stage1.seg file
+    listName=`basename ${LIST}`
+    mkdir ${OUTPUT_DIR}/${listName}
+    rm -f ${OUTPUT_DIR}/${listName}/stage1.seg
+    for URI in `cat $LIST`
+    do
+      cat ${OUTPUT_DIR}/${URI}.MPG.stage1.seg >> ${OUTPUT_DIR}/${listName}/stage1.seg 
+    done
+    
+    # Cross-show CLR diarization
+    ${SID} -ubmset pakita -iter 5 -stop 1.0 -cross -plpdir ${PLP_DIR} ${INPUT_DIR}/%s.wav ${OUTPUT_DIR}/${listName} ${OUTPUT_DIR}/${listName}/stage1.seg
+
+    # Format conversion
+    ${SEG2MDTM} < ${OUTPUT_DIR}/${listName}/cross.stop1.0.seg > ${OUTPUT_DIR}/${listName}/diarization.mdtm
 fi
 
